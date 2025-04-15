2655562c9c2c3b7d12b732174ec02fbe462cbeb9
fernando <413114463@qq.com>
2025-04-15 16:34:52 +0800

    seed file

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,18 +1,18 @@
-const prismaClient = require("../prismaClient.js");
+import prisma from "../utils/prisma.js";
 
 async function rawSql() {
-  // const result =
-  //   await prismaClient.$executeRaw`INSERT INTO Product(name, updateAt) values ('trousers221', '2021-01-02');`;
-  // // await prismaClient.$executeRaw`INSERT INTO "User" ("email", "username", "password") VALUES ('555666@example.com', 'johnson', '555666') ON CONFLICT DO NOTHING;`;
+  prisma.$executeRaw`insert into gallery2.Platform (name) values ('线下'), ('闲鱼'), ('拼多多'), ('抖音');`;
+  prisma.$executeRaw`insert into gallery2.Express (name) values ('顺丰'), ('韵达'), ('中通'), ('百世');`;
+  // await prismaClient.$executeRaw`INSERT INTO "User" ("email", "username", "password") VALUES ('555666@example.com', 'johnson', '555666') ON CONFLICT DO NOTHING;`;
   // console.log({ result });
 }
 
 rawSql()
   .then(async () => {
-    await prismaClient.$disconnect();
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
     console.error(e);
-    await prismaClient.$disconnect();
+    await prisma.$disconnect();
     process.exit(1);
   });
