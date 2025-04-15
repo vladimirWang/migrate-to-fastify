caac5f17c0757bfe2b39c7ba790910cc8bc8d25e
fernando <413114463@qq.com>
2025-04-15 16:50:56 +0800

    排查seed失败

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,8 +5,20 @@ async function rawSql() {
   const result2 = prisma.$executeRaw`insert into gallery2.Express (name) values ('顺丰'), ('韵达'), ('中通'), ('百世');`;
   // await prismaClient.$executeRaw`INSERT INTO "User" ("email", "username", "password") VALUES ('555666@example.com', 'johnson', '555666') ON CONFLICT DO NOTHING;`;
   // console.log({ result });
-  console.log("result1: ", result1);
-  console.log("result2: ", result2);
+  result1
+    .then((res) => {
+      console.log("result1 then: ", res);
+    })
+    .catch((err) => {
+      console.log("result1 err: ", err);
+    });
+  result2
+    .then((res) => {
+      console.log("result2 then: ", res);
+    })
+    .catch((err) => {
+      console.log("result2 err: ", err);
+    });
 }
 
 rawSql()
